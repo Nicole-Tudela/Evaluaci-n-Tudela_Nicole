8307cc9c4128e493d95bfb8f4292c0a92a84b182
Edna-Moda <ni.tudela@duocuc.cl>
2025-07-02 11:20:00 -0400

    Evaluación-Código.2

---
+++ codigo/funciones.py
@@ -0,0 +1,37 @@
+from os import system
+from msvcrt import getch
+
+
+nombre = {}
+codigo = {}
+Menu = []
+
+
+while True:
+        
+    print (input("Ingrese nombre del Producto : "))
+    print (input("Ingrese codigo del Producto"))
+        
+    if registrar_producto == {nombre}:
+     print ("Nombre registrado ")
+    if registrar_producto == {codigo}:
+      print (" El codigo ingresado es valido ")
+    
+    
+    
+    
+    def registrar_producto():
+        pass
+            
+    def Listar_productos_con_stock():
+     pass    
+     
+    def Eliminar_producto_por_codigo():
+        print (input("Ingrese codigo del producto : "))
+        if codigo == codigo.pop: 
+                print (" Producto eliminado ")
+        else:
+                print("El producto que desea eliminar no existe, ingrese otro codigo ")
+
+
+
--- codigo/main.py
+++ codigo/main.py
@@ -4,7 +4,24 @@ import funciones
 Menu = """
 
 
-1. Registrar pruducto
-2. Listar productos
+===================
+  Menu de Opciones
+====================
+1. Registrar Producto 
+2. Listar productos con stock
 3. Eliminar producto por codigo
-4. Salir      """
+4. Salir """
+
+
+while True:
+   print (input("Ingrese una opcion : "))
+   
+   if opcion ==1: 
+            pass
+   if opcion ==2:
+            pass
+   if opcion ==3:
+            pass
+   if opcion ==4:
+    break
+   
